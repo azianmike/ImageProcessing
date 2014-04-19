e078b280aa22d2d94aa0096ec8971c3cf6d940f9
michaelluo <michaeluo@gmail.com>
2014-04-19 14:27:33 -0500

    Accepting connections!

--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@ __author__ = 'michaelluo'
 
 from ImageCompare import compareImagesToDB
 import time
-from PIL import Image
+import socket
 
 test_pic = 'SimilarNoFlash.jpg' #testing this pic against all pics in our db;
 db = [
@@ -19,19 +19,43 @@ db = [
     [0, 0, 'Similar1.jpg']
     ]
 
-start_time = time.time()
-#print db[0][2]
+def getSimilarImages():
+    start_time = time.time()
 
-returnMe, db2 = compareImagesToDB(db.pop()[2], db)
-print 'returned that are similar ' + str(returnMe)
-print 'returned that are not' + str(db2)
-compareImagesToDB(db2.pop()[2], db2)
-print '--------------------'
-compareImagesToDB(test_pic, db2)
-print '---------------------'
-compareImagesToDB(test_pic, db2)
-print '---------------------'
-compareImagesToDB(test_pic, db)
+# returnMe, db2 = compareImagesToDB(db.pop()[2], db)
+# print 'returned that are similar ' + str(returnMe)
+# print 'returned that are not' + str(db2)
+# compareImagesToDB(db2.pop()[2], db2)
+# print '--------------------'
+# compareImagesToDB(test_pic, db2)
+# print '---------------------'
+# compareImagesToDB(test_pic, db2)
+# print '---------------------'
+# compareImagesToDB(test_pic, dbtest)
+    while(len(db)!=0):
+        returnMe, db = compareImagesToDB(db.pop()[2], db)
+        print 'returned that are similar ' + str(returnMe)
+        print 'returned that are not' + str(db)
 
+    print str(time.time() - start_time) + " seconds"
 
-print str(time.time() - start_time) + " seconds"
+#create an INET, STREAMing socket
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#bind the socket to a public host,
+# and a well-known port
+serversocket.bind((socket.gethostname(), 5069))
+#become a server socket
+serversocket.listen(5)
+print 'listening'
+while 1:
+    #accept connections from outside
+    (conn, address) = serversocket.accept()
+    print 'accepted'
+    print conn
+    print 'listening for data'
+    data = conn.recv(1024)
+    print 'done listening for data'
+    print data
+    if not data: break
+    conn.sendall(data)
+    conn.close()
